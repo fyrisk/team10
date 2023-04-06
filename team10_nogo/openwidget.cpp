c603d963b05fd8eeb79d957f95edbff54b78b8f4
fyrisk <3410613406@qq.com>
2023-04-06 21:39:37 +0800

    删了导进仓库的几个多余的文件

--- team10_nogo/openwidget.cpp
+++
@@ -1,7 +0,0 @@
-#include "openwidget.h"
-
-OpenWidget::OpenWidget(QWidget *parent) : QWidget(parent)
-{
-    beginButton = new QPushButton("Begin", this);
-    connect(beginButton, &QPushButton::clicked, this, &OpenWidget::beginButtonClicked);
-}
